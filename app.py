37394c7ff658e4f1b5757558b532c91a075dda98
Ramsai Kolnati <ramsaikolnati304@gmail.com>
2026-03-03 22:36:16 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -75,7 +75,3 @@ def recommend_get(query: str):
         "recommendations": results
     }
 
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
